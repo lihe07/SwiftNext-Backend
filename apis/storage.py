7b61cff0bc6f58a46c2803b87fa8360a4a1d172b
lihe <lihelihe2007@qq.com>
2022-01-25 20:48:22 +0800

    UPD

--- apis/storage.py
+++ apis/storage.py
@@ -6,6 +6,8 @@ import uuid
 
 from sanic import Sanic, Request, HTTPResponse, response, json
 from sanic.log import logger
+from sanic.response import ResponseStream
+
 from apis import perm
 import config
 import mimetypes
@@ -60,7 +62,7 @@ async def upload(request: Request) -> HTTPResponse:
 
 
 @app.get("/storage/inline/<fid>")
-async def get_inline(request: Request, fid: str) -> HTTPResponse:
+async def get_inline(request: Request, fid: str):
     """
     获取附件的内联链接
     """
@@ -87,4 +89,40 @@ async def get_inline(request: Request, fid: str) -> HTTPResponse:
     return await response.file_stream(result["local_path"], mime_type=result["mime_type"], chunk_size=1024, headers={
         "Content-Disposition": "inline; filename=\"{}\"".format(result["name"]),
         "Content-Type": result["mime_type"],
+        "Cache-Control": "max-age=86400",  # 控制缓存 1天
+    })
+
+
+@app.get("/storage/download/<fid>")
+async def get_download(request: Request, fid: str):
+    """
+    以附件的形式发送某个文件
+    :param request:
+    :param fid:
+    :return:
+    """
+    if fid is None:
+        return json({
+            "code": 4,
+            "message": {
+                "cn": "请指定一个文件ID",
+                "en": "Please specify a file ID"
+            }
+        }, 400)
+    result = config.database().storage.find_one({"_id": fid})
+    if result is None:
+        return json({
+            "code": 4,
+            "message": {
+                "cn": "指定的文件不存在",
+                "en": "The specified file does not exist"
+            },
+            "description": {
+                "file_id": fid
+            }
+        }, 404)
+    return await response.file_stream(result["local_path"], mime_type=result["mime_type"], chunk_size=1024, headers={
+        "Content-Disposition": "attachment; filename=\"{}\"".format(result["name"]),
+        "Content-Type": result["mime_type"],
+        "Cache-Control": "max-age=86400",  # 控制缓存 1天
     })
